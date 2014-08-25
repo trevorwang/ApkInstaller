682270a639c2a526dbcf7fa4b0eabb4e5c87de4d
Ming Wang <mingwan@cisco.com>
2014-08-25 10:50:45 +0800

    Initial Commit

---
+++ ApkInstaller/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ApkInstaller
+//
+//  Created by MingWang on 8/25/14.
+//  Copyright (c) 2014 MingWang. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
